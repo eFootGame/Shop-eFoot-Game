a791a7dad3aec8edd7f6e87caafdc33719f39052
eFoot Game <dicord22samp@gmail.com>
2026-01-12 19:33:48 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAM7gLKuLRfhFdWyakFS1jU4c8xU1fg-FU",
+  authDomain: "family-bank-966ae.firebaseapp.com",
+  databaseURL: "https://family-bank-966ae-default-rtdb.firebaseio.com",
+  projectId: "family-bank-966ae",
+};
+firebase.initializeApp(firebaseConfig);
